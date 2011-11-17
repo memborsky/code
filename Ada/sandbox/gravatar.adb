3bd56c01b72ee81af0ed7ea7df4921749c8eb5ba
Matt Emborsky <memborsky@gmail.com>
2011-11-17 16:57:14 -0500

    [Ada - gravatar] Converting all keywords from 'Keyword' to 'keyword'

--- Ada/sandbox/gravatar.adb
+++ Ada/sandbox/gravatar.adb
@@ -1,42 +1,42 @@
-With Ada.Text_IO;
-With GNAT.MD5;
-With Ada.Command_Line;
-With Ada.Strings.Unbounded;
+with Ada.Text_IO;
+with GNAT.MD5;
+with Ada.Command_Line;
+with Ada.Strings.Unbounded;
 
-Procedure gravatar Is
+procedure gravatar is
 
     uri         : String := "https://secure.gravatar.com/avatar/";
     size        : Integer := 140;
 
-    Function Get_Integer_As_String (Input : Integer) Return String Is
-    
+    function Get_Integer_As_String (Input : Integer) return String is
+
         Result : String := Integer'Image(Input);
 
-    Begin
-    
-        Return Result(Result'First + 1 .. Result'Last);
-        
-    End Get_Integer_As_String;
+    begin
 
-Begin
+        return Result(Result'First + 1 .. Result'Last);
 
-    If Ada.Command_Line.Argument_Count >= 1 Then
+    end Get_Integer_As_String;
 
-        For Argument In 1..Ada.Command_Line.Argument_Count Loop
+begin
 
-            Declare
+    if Ada.Command_Line.Argument_Count >= 1 then
+
+        for Argument In 1..Ada.Command_Line.Argument_Count loop
+
+            declare
 
                 email : Ada.Strings.Unbounded.Unbounded_String := Ada.Strings.Unbounded.To_Unbounded_String(Ada.Command_Line.Argument(Argument));
 
-            Begin
+            begin
 
                 Ada.Text_IO.Put_Line(uri & GNAT.MD5.Digest(Ada.Strings.Unbounded.To_String(email)) & "?s=" & Get_Integer_As_String(size));
 
-            End;
-              
-        End Loop;
-    End If;
-        
+            end;
 
-End gravatar;
+        end loop;
+    end if;
+
+
+end gravatar;
 
