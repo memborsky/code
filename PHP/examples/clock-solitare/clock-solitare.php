50482d2456119372a84067a7454698039befa4d9
Matt Emborsky <memborsky@gmail.com>
2016-02-24 16:01:03 -0500

    PHP: Clock Solitare. This adds the layer wise suit checking to the game to follow the proper rules of the game.

--- PHP/examples/clock-solitare/clock-solitare.php
+++ PHP/examples/clock-solitare/clock-solitare.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Setup:
+ * Use one deck of cards. Shuffle it and you are ready to go. Imagine a clock on your game table. All ones will be placed at the 1 o'clock position, all Queens on 12 o'clock, and the kings in the center. This game has no cascades. In the figure the stock and waste are inside the clock. If playing the game with a real deck you would hold the stock in your hand and put the waste at a convenient location outside the clock.
+ *
+ * Rules:
+ * Draw card by card from the stock. If one fits to any of the 13 foundations play it, otherwise put it on the waste. Foundations are built by alternating color and layer wise. This means that you can choose your starting suit, but the second card on the foundation must be opposite color. Layer wise by suit means that you must have the same suit on each layer of the foundations. So on all foundations you must have - for example - Spades, Diamonds, Clubs, Hearts in the same order, though you can choose the order during the first moves of the game. The game allows you to recycle the waste stack exactly two times.
+**/
+
 namespace ClockSolitare;
 
 include "class.decks.php";
@@ -17,14 +25,14 @@ $suit_colors = array
 
 $deck = null;
 $piles = null;
-
+$layers = null;
 
 /**
  * Setup a new game for us to play.
  */
 function create_new_game()
 {
-    global $deck, $piles;
+    global $deck, $piles, $layers;
 
     $deck = new Decks();
     $piles = array();
@@ -38,6 +46,8 @@ function create_new_game()
     }
 
     $piles["W"] = new Piles();
+
+    $layers = array();
 }
 
 
@@ -48,6 +58,7 @@ function destroy_game()
 {
     unset($deck);
     unset($piles);
+    unset($layers);
 
     // Force garbage collection
     gc_collect_cycles();
@@ -59,26 +70,48 @@ function destroy_game()
  */
 function play_card($card)
 {
-    global $piles, $suit_colors;
+    global $piles, $suit_colors, $layers;
 
     $card_face = $card->GetFace();
     $card_suit = $card->GetSuit();
 
-    /**
-     * Discard to wasted pile if both are true:
-     * 1.) the pile we are playing onto is not empty
-     * 2.) the pile's top card suit matches ours in color
-     */
-    if (
-            !$piles[$card_face]->isEmpty()
-            && $suit_colors[$piles[$card_face]->top()->GetSuit()] == $suit_colors[$card->GetSuit()]
-        )
+    $pile_count = $piles[$card_face]->count();
+
+    if (isset($layers[$pile_count]))
     {
-        $piles["W"]->push($card);
-        return;
+        // The current layer has been set.
+
+        if ($layers[$pile_count] == $card_suit)
+        {
+            $piles[$card_face]->push($card);
+        }
+        else
+        {
+            $piles["W"]->push($card);
+        }
+    }
+    else
+    {
+        // The card could be the new layer.
+
+        if ($pile_count == 0)
+        {
+            // This means we have found the first card in the game.
+
+            $layers[$pile_count] = $card_suit;
+            $piles[$card_face]->push($card);
+        }
+        elseif ($suit_colors[$piles[$card_face]->top()->GetSuit()] != $suit_colors[$card_suit])
+        {
+            $layers[] = $card_suit;
+            $piles[$card_face]->push($card);
+        }
+        else
+        {
+            $piles["W"]->push($card);
+        }
     }
 
-    $piles[$card_face]->push($card);
 }
 
 
@@ -106,12 +139,18 @@ while (strtolower(readline("Would you like to play a new game? (y/n) ")) == "y")
     }
 
 
-    $output = "";
+    $output = "Layer :  1   2   3   4\n";
+    $output .= "   --------------------\n";
 
     // Output the results of the game.
     foreach ($piles as $face => $pile)
     {
-        $output .= str_pad($face, 4, " ", STR_PAD_LEFT) . " : ";
+        if ($face == "W")
+        {
+            $output .= "   --------------------\n";
+        }
+
+        $output .= str_pad($face, 5, " ", STR_PAD_LEFT) . " : ";
 
         for ($pile->rewind(); $pile->valid(); $pile->next())
         {
